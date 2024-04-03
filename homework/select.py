af8bdd09996184f9a252c0a9b508d3af8a22f88d
Akobir990999 <165624598+Akobir990999@users.noreply.github.com>
2024-04-03 09:21:18 +0500

    Add files via upload

---
+++ homework/select.py
@@ -0,0 +1,59 @@
+# a = int(input("a sonni kirit:"))
+# p = a*4
+# print(p)
+
+# b = 12,23
+# print(max(b))
+
+# s = int(input("son kirit:"))
+# if s==1:
+#     print("dushanba")
+# elif s==2:
+#     print("seshanba")
+# elif s==3:
+#     print("chorshanba")
+# elif s==4:
+#     print("payshanba")
+# elif s==5:
+#     print("juma")
+# elif s==6:
+#     print("shanba")
+# elif a==7:
+#     print("yakshanba")
+# else:
+#     print("son yettiga teng yoki kichik bolsin")
+
+# def daraja(a,b,c,d,e):
+#     p = a,b,c,d,e
+#     for i in p:
+#         print(i**3)
+# daraja(12,23,45,56,67)
+
+# davlatlar = ["germaniya","fransiya","belgiya","finlandiya","ispaniya"]
+# print(davlatlar)
+
+# ism = ["ali","vali","sanjar","sardor","alisher","hahaha"]
+# for i in ism:
+#     n = 0
+#     print("seni yaxshi odam bolishinga ishonaman",i)
+# print(f"kod {len(ism)} marta takrorlandi")
+
+# a = int(input("son kirit:"))
+# b = int(input("son kirit:"))
+# if a == b:
+#     print("sonlar teng ")
+
+# a = int(input("son kirit:"))
+# if a > 0:
+#     print(f"{a} son musbat")
+# elif a < 0:
+#     print(f"{a} son manfiy")
+
+# a = int(input("yoshingni kirit:"))
+# if a < 4 or a > 60:
+#     print("bepul")
+# elif a < 18:
+#     print("10 ming som")
+# elif a > 18:
+#     print("20 ming som")   
+
