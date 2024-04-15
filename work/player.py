117ca89d0af40c4772253752c27920e3ffe9cb07
Rios_Nl <165624598+Akobir990999@users.noreply.github.com>
2024-04-15 09:56:30 +0500

    Add files via upload

---
+++ work/player.py
@@ -0,0 +1,31 @@
+# a = lambda a,b:a*b
+# print(a(12,12))
+
+# def k(a):
+#     return a*a
+# def kv():
+#     print("hi bu kvadratni topadigan funksiya")
+#     (lambda a:print(a*a))(12)
+# kv()
+
+# ls = input()
+# print(ls.split())
+
+# def aa(a):
+#     return a*a
+# a,b = map(aa,[2,4])
+# print(a,b)
+
+# def map1(func,ls):
+#     for i in ls:
+#         print(func(i),end=" ")
+# map1(lambda a:a*a,[1,2,3,4,5,6])
+
+# a = lambda name:f"mening ismim {name}"
+# print(a("akobir"))
+# (lambda ism,yosh:print(f"{ism} siz {2024-yosh}-yilda tugilgansiz"))("akobir",12)
+# (lambda son:print(son**2,son**3))(12)
+# (lambda son1: print("juft son") if son1%2==0 else print("toq son"))(13)
+# (lambda son2,son3:print(son2) if son2 > son3 else print("teng") if son2==son3 else print(son3))(3,3)
+# (lambda x,y=2:print(x**y))(2)
+# (lambda son4:[print(f"{son4} son {i}ga qoldiqsiz bolinadi")for i in range(2,10+1)  if son4%i==0 ] )(70)
