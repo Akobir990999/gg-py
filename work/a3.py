417d71df28ffa5747f45a7e401f66f1999705832
Rios_Nl <165624598+Akobir990999@users.noreply.github.com>
2024-04-24 09:27:10 +0500

    Add files via upload

---
+++ work/a3.py
@@ -0,0 +1,243 @@
+# class kvadrat:
+#     def __init__(self,a):
+#         self.a = a
+
+#     def pr(self):
+#         p = 4*self.a
+#         print(p)
+
+# kv = kvadrat(6)
+# kv.pr()
+#2
+# class kvadrat:
+#     def __init__(self,a):
+#         self.a = a
+
+#     def s(self):
+#         s = self.a**2
+#         print(s)
+
+# kv = kvadrat(6)
+# kv.s()
+#3
+# class togri_tortburchak:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+
+#     def pr(self):
+#         p = (self.a+self.b)*2
+#         print(p)
+    
+#     def s(self):
+#         s = self.a*self.b
+#         print(s)
+
+# tt = togri_tortburchak(12,12)
+# tt.pr()
+# tt.s()
+#4
+# class diametr:
+#     def __init__(self,d):
+#         self.d = d
+#         self.p = 3.14
+
+#     def uzunlik(self):
+#         l = self.p*self.d
+#         print(l)
+    
+# dm = diametr(12)
+# dm.uzunlik()
+#5
+# class kub:
+#     def __init__(self,a):
+#         self.a = a
+
+#     def hajm(self):
+#         v = self.a**3
+#         print(v)
+    
+#     def tola_sirt(self):
+#         s = 6*self.a**2
+#         print(s)
+
+# kb = kub(12)
+# kb.hajm()
+# kb.tola_sirt()
+#6
+# class paralelepepid:
+#     def __init__(self,a,b,c):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+
+#     def hajm(self):
+#         v = self.a * self.b * self.c
+#         print(v)
+
+#     def tola_sirti(self):
+#          s = 2*(self.a*self.b+self.b*self.c+self.a*self.c)
+#          print(s)
+
+# pr = paralelepepid(3,3,3)
+# pr.tola_sirti()
+# pr.hajm()
+#7
+# class radio:
+#     def __init__(self,r):
+#         self.r = r
+#         self.p = 3.14
+
+
+#     def uzunlik(self):
+#         l = 2*self.p*self.r
+#         print(l)
+
+#     def yuza(self):
+#         s= self.p*self.r**2
+#         print(s)
+
+# rd = radio(12)
+# rd.yuza()
+# rd.uzunlik()
+#8
+# class orta:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+
+#     def orta_ar(self):
+#         d = (self.a+self.b)/2
+#         print(d)
+
+
+# ar = orta(19,17)
+# ar.orta_ar()
+#9
+# class geo:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+
+#     def orta_geometrigi(self):
+#         i = (self.a*self.b)**0.5
+#         print(i)
+
+# g = geo(9,98)
+# g.orta_geometrigi()
+#10
+
+# class son:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+
+#     def qosh(self):
+#         print(self.a+self.b)
+
+#     def ayir(self):
+#         print(self.a-self.b)
+    
+#     def kvadrati(self):
+#         print(self.a*self.a)
+#         print(self.b*self.b)
+
+# sn = son(12,12)
+# sn.ayir()
+# sn.qosh()
+# sn.kvadrati()
+
+#11
+# class son:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+
+#     def qosh(self):
+#         print(self.a+self.b)
+
+#     def kopaytir(self):
+#         print(self.a*self.b)
+
+
+#     def moduli(self):
+#         print(abs(self.a))
+#         print(abs(self.b))
+      
+
+# sn = son(12,12)
+# sn.qosh()
+# sn.moduli()
+#12
+# class kotetlar:
+#     def __init__(self,a,b):
+#         self.a = a
+#         self.b = b
+
+#     def gipotenuzasi(self):
+#         c = (self.a**2+self.b**2)**0.5
+#         print(c)
+#         return c
+
+#     def pr(self):
+#         p = self.a+self.b+self.gipotenuzasi()
+
+
+# kt = kotetlar(12,12)
+# kt.gipotenuzasi()
+# kt.pr()
+#13
+# class radio:
+#     def __init__(self,R1,R2):
+#         self.R1 = R1
+#         self.R2 = R2
+#         self.p = 3.14
+
+#     def s1(self):
+#         S1 = self.p*self.R1
+#         print(S1)
+
+#     def s2(self):
+#         S2 = self.p*self.R2
+#         print(S2)
+
+#     def s3(self):
+#         S3 = self.p*(self.R1-self.R2)
+#         print(S3)
+
+# rd = radio(12,23)
+# rd.s1()
+# rd.s2()
+# rd.s3()
+#14
+# class uzunlik:
+#     def __init__(self,r):
+#         self.r = r
+#         self.p = 3.14
+
+
+#     def radio(self):
+#         l = 2*self.p*self.r
+#         print(l)
+
+#     def yuza(self):
+#         s = self.p*self.r**2
+#         print(s)
+
+# uz = uzunlik(12)
+# uz.radio()
+# uz.yuza()
+
+
+
+
+
+
+
+    
+
+
+
+
+
+    
